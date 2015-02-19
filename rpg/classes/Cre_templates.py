cb34d251db9ce2635efb5d24faeea23c58e6a47c
Joseph Ray <joseph.h.ray@gmail.com>
2015-02-19 23:06:26 +1000

    added a creature template

---
+++ rpg/classes/Cre_templates.py
@@ -0,0 +1,8 @@
+## Creature templates ##
+
+noun = ["warrior", "pillow", "zombie", "gremlin", "kitten", "bunny"]
+adjective = ["fluffy", "biscuit", "gingerbread", "gummy", "boss rolla", "short", "bandaged"]
+mood = ["friendly looking", "angry", "berserk", "mean looking", "seductive"]
+nationality = ["german", "russian", "english", "scandanavian", "french", "italian"]
+amount = [1,3,5,10,15,20,25,30,50,100]
+weapon = ["balloons", "swords", "lazers", "bows", "nunchuks", "guitar (ax)", "cheese grater", "flame thrower"]
