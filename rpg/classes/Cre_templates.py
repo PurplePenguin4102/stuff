2324f1121bee57a698677d507cde582a5dfe8d18
Joey Ray <joseph.h.ray@gmail.com>
2015-02-20 17:24:08 +1000

    Delete Cre_templates.py

--- rpg/classes/Cre_templates.py
+++
@@ -1,8 +0,0 @@
-## Creature templates ##
-
-cre_noun = ["warrior", "pillow", "zombie", "gremlin", "kitten", "bunny", "old-timey strongman"]
-cre_adjective = ["fluffy", "gingerbread", "gummy", "short", "bandaged", "victorian", "georgian"]
-cre_mood = ["friendly looking", "angry", "berserk", "mean looking", "seductive", "hulk-mode"]
-cre_nationality = ["german", "russian", "english", "scandanavian", "french", "italian", "ethiopian"]
-cre_amount = [1,3,5,10,15,20,25,30,50,100]
-cre_weapon = ["balloons", "swords", "lazers", "bows", "nunchuks", "guitar (ax)", "cheese grater", "flame thrower"]
